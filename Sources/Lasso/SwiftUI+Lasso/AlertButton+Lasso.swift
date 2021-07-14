2919126dd92d255f41f81141e19467aa3cd4ace5
Charles Pisciotta <charles.pisciotta@ww.com>
2021-07-14 15:14:30 -0400

    Add Lasso support to default Alert.Button

---
+++ Sources/Lasso/SwiftUI+Lasso/AlertButton+Lasso.swift
@@ -0,0 +1,31 @@
+//
+// ==----------------------------------------------------------------------== //
+//
+//  SwiftUI+Lasso.swift
+//
+//  Created by Charles Pisciotta on 07/14/2021
+//
+//
+//  This source file is part of the Lasso open source project
+//
+//     https://github.com/ww-tech/lasso
+//
+//  Copyright © 2019-2021 WW International, Inc.
+//
+// ==----------------------------------------------------------------------== //
+//
+
+#if canImport(SwiftUI)
+import SwiftUI
+import Combine
+
+@available(iOS 13.0, OSX 10.15, tvOS 13.0, watchOS 6.0, *)
+extension Alert.Button {
+
+    public static func `default`<Target: ActionDispatchable>(label: Text, for target: Target, action: Target.Action) -> Self {
+        self.default(label) { target.dispatchAction(action) }
+    }
+    
+}
+
+#endif
