c9b1e920aea167fbb845a3f8a01e1c25c717906f
Steven Grosmark <steven.grosmark@ww.com>
2022-03-31 09:17:24 -0400

    Change approach to FlowTestCase setUp and tearDown

--- Sources/LassoTestUtilities/FlowTestCase.swift
+++ Sources/LassoTestUtilities/FlowTestCase.swift
@@ -24,34 +24,80 @@ import UIKit
 /// 2) convenience controllers in the root of the window which cover the most common use cases
 open class FlowTestCase: XCTestCase {
     
-    public private(set) var window: UIWindow!
-    public private(set) var navigationController: UINavigationController!
-    public private(set) var rootController: UIViewController!
-    
-    open override func setUpWithError() throws {
-        try super.setUpWithError()
+    public var window: UIWindow {
         setUpFlowTestCase()
+        return _window
     }
     
-    open override func setUp() {
-        super.setUp()
+    public var navigationController: UINavigationController {
         setUpFlowTestCase()
+        return _navigationController
     }
+    
+    public var rootController: UIViewController {
+        setUpFlowTestCase()
+        return _rootController
+    }
+    
+    private var _window: UIWindow!
+    private var _navigationController: UINavigationController!
+    private var _rootController: UIViewController!
+    
+    // https://developer.apple.com/documentation/xctest/xctestcase/set_up_and_tear_down_state_in_your_tests
+    //
+    // setUp:
+    // XCTest runs the setup methods once before each test method starts:
+    //  setUp() async throws (Xcode 13+) first,
+    //  then setUpWithError() (Xcode 11.4+),
+    //  then setUp() (Xcode 7.5+).
+    //
+    // We want to set up the FlowTestCase as early as possible so that
+    // subclasses can start to access the helpers and make assertions in
+    // their setUp funcs.
+    // Since subclasses _might_ want to use the async version of setUp,
+    // and the @available runtime check won't work (needs a compile time check),
+    // we can't override the various set up funcs to guarantee initialization.
+    // Instead, we'll use lazy initialization of the window and controller properties.
+    //
+    // tearDown:
+    // XCTest runs the teardown methods once after each test method completes, with
+    //  tearDown() first (Xcode 7.5+),
+    //  then tearDownWithError() (Xcode 11.4+),
+    //  then tearDown() async throws (Xcode 13+).
+    //
+    // We want to tear down the FlowTestCase as late as possible, since
+    // subclasses _might_ depend on these values in their tearDown funcs
+    // (Rarely done, but possible to use XCTest assertions in tearDown).
+    // Skipping the async version of tearDown because the @available runtime check
+    // is insufficient - it needs a compiler check.
+    
+    #if compiler(>=5.2)
+    open override func tearDownWithError() throws {
+        tearDownFlowTestCase()
+        try super.tearDownWithError()
+    }
+    
+    #else
+    open override func tearDown() {
+        tearDownFlowTestCase()
+        super.tearDown()
+    }
+    #endif
     
     private func setUpFlowTestCase() {
-        guard window == nil else { return }
-        window = UIWindow()
-        window.makeKeyAndVisible()
-        rootController = UIViewController()
-        navigationController = UINavigationController(rootViewController: rootController)
-        window.rootViewController = navigationController
-        waitForEvents(in: window)
-        
-        addTeardownBlock { [weak self] in
-            self?.window = nil
-            self?.navigationController = nil
-            self?.rootController = nil
-        }
+        guard _window == nil else { return }
+        _window = UIWindow()
+        _window.makeKeyAndVisible()
+        _rootController = UIViewController()
+        _navigationController = UINavigationController(rootViewController: _rootController)
+        _window.rootViewController = _navigationController
+        waitForEvents(in: _window)
+    }
+    
+    private func tearDownFlowTestCase() {
+        _window = nil
+        _navigationController = nil
+        _rootController = nil
     }
     
 }
