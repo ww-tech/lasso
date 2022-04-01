b4edf0d21aa85c595d7a06829b296aba6a38b990
Steven Grosmark <steven.grosmark@ww.com>
2022-04-01 10:42:33 -0400

    Remove support for async setUp and tearDown

--- Sources/LassoTestUtilities/FlowTestCase.swift
+++ Sources/LassoTestUtilities/FlowTestCase.swift
@@ -19,9 +19,12 @@ import XCTest
 import UIKit
 
 /// Provides conveniences for Flow unit testing.
+///
 /// Key features:
 /// 1) a UIWindow which is made 'key and visible'. This is required for lifecycle hooks to work.
 /// 2) convenience controllers in the root of the window which cover the most common use cases
+///
+/// Does **NOT** support async versions of `setUp` and `tearDown`
 open class FlowTestCase: XCTestCase {
     
     public var window: UIWindow {
@@ -42,6 +45,7 @@ open class FlowTestCase: XCTestCase {
     private var _window: UIWindow!
     private var _navigationController: UINavigationController!
     private var _rootController: UIViewController!
+    private var _didTearDown: Bool = false
     
     // https://developer.apple.com/documentation/xctest/xctestcase/set_up_and_tear_down_state_in_your_tests
     //
@@ -54,11 +58,19 @@ open class FlowTestCase: XCTestCase {
     // We want to set up the FlowTestCase as early as possible so that
     // subclasses can start to access the helpers and make assertions in
     // their setUp funcs.
-    // Since subclasses _might_ want to use the async version of setUp,
-    // and the @available runtime check won't work (needs a compile time check),
-    // we can't override the various set up funcs to guarantee initialization.
-    // Instead, we'll use lazy initialization of the window and controller properties.
-    //
+    
+    #if compiler(>=5.2)
+    open override func setUpWithError() throws {
+        try super.setUpWithError()
+        setUpFlowTestCase()
+    }
+    #endif
+    
+    open override func setUp() {
+        super.setUp()
+        setUpFlowTestCase()
+    }
+    
     // tearDown:
     // XCTest runs the teardown methods once after each test method completes, with
     //  tearDown() first (Xcode 7.5+),
@@ -68,8 +80,6 @@ open class FlowTestCase: XCTestCase {
     // We want to tear down the FlowTestCase as late as possible, since
     // subclasses _might_ depend on these values in their tearDown funcs
     // (Rarely done, but possible to use XCTest assertions in tearDown).
-    // Skipping the async version of tearDown because the @available runtime check
-    // is insufficient - it needs a compiler check.
     
     #if compiler(>=5.2)
     open override func tearDownWithError() throws {
@@ -85,7 +95,7 @@ open class FlowTestCase: XCTestCase {
     #endif
     
     private func setUpFlowTestCase() {
-        guard _window == nil else { return }
+        guard _window == nil, !_didTearDown else { return }
         _window = UIWindow()
         _window.makeKeyAndVisible()
         _rootController = UIViewController()
@@ -95,6 +105,7 @@ open class FlowTestCase: XCTestCase {
     }
     
     private func tearDownFlowTestCase() {
+        _didTearDown = true
         _window = nil
         _navigationController = nil
         _rootController = nil
